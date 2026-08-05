e6e51ba2803aa64160cd891b77d93c62d25a1463
student-Imran <ahmedimranmd55@gmail.com>
2026-08-05 16:45:54 +0600

    User Login authentication successfully added with jsonwebtoken

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { Application , Request,Response } from "express";
 import cors from "cors";
 import config from "./config";
 import { userRoutes } from "./modules/user/user.route";
+import { authRoutes } from "./modules/auth/auth.route";
 const app : Application = express();
 
 app.use(cors({
@@ -20,6 +21,7 @@ app.get("/", (req : Request, res : Response) => {
 });
 
 app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
 
 
 
