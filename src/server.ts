03eaab01465237bc1e6eba70119b557df8f87f93
student-Imran <ahmedimranmd55@gmail.com>
2026-08-05 11:24:19 +0600

    Prisma connected and config env file successfully

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
 
-const PORT = process.env.PORT || 3000;
+const PORT = config.port
 
 async function main() {
     try{
+        await prisma.$connect();
+        console.log("Connected to the database successfully.");
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
     }
     catch(err){
         console.error("Error occurred while starting the server:", err);
+        await prisma.$disconnect();
         process.exit(1);
     }
 }
