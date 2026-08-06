9852f6432f8c7dbc1243a197dbc9c5ec5747a814
student-Imran <ahmedimranmd55@gmail.com>
2026-08-06 17:10:38 +0600

    Implement auth middleware successfully

--- src/modules/properties/property.route.ts
+++ src/modules/properties/property.route.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { propertyController } from "./property.controller";
+import { Role } from "../../../generated/prisma/enums";
+import { auth } from "../../middlewares/auth";
 
 const router = Router();
 
-router.post("/properties", propertyController.createProperty);
+router.post("/properties", 
+    auth(Role.LANDLORD),
+    propertyController.createProperty);
 
 export const propertyRoutes = router;
